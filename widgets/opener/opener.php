db9ee5f081804cbdc5e22bfe1f189b04c0f9295b
Bruno Meilick <b@bnomei.com>
2017-05-22 19:12:25 +0200

    fixed widget license link
    Signed-off-by: Bruno Meilick <b@bnomei.com>

--- widgets/opener/opener.php
+++ widgets/opener/opener.php
@@ -8,7 +8,7 @@ return [
   ],
   'html' => function() {
     return tpl::load(__DIR__ . DS . 'opener.html.php', array(
-      'text' => '<b>Kirby Opener</b> is running in trial mode. Please support the development of this plugin and <a href="https://sites.fastspring.com/bnomei/product/kirby-opener" target="_blank">buy a license</a>. If you already have a license key, please add it to your <code title="site/config/config.php" style="border-bottom: 1px dotted; font-family: monospace;">config.php</code> file.',
+      'text' => '<b>Kirby Opener</b> is running in trial mode. Please support the development of this plugin and <a href="https://bnomei.onfastspring.com/kirby-opener" target="_blank">buy a license</a>. If you already have a license key, please add it to your <code title="site/config/config.php" style="border-bottom: 1px dotted; font-family: monospace;">config.php</code> file.',
     ));
   }
 ];
